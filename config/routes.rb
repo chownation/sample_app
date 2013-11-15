038330fe9634063255f3b2f45706b0223e503959
chownation <michael.chow19@gmail.com>
2013-11-15 10:24:19 -0500

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 SampleApp::Application.routes.draw do
 
   resources :users
-
+  resources :sessions, only: [:new, :create, :destroy]
   root 'static_pages#home'
-  match '/signup', to: 'users#new', via: 'get'
-  match '/help', to: 'static_pages#help', via: 'get'
-  match '/about', to: 'static_pages#about', via: 'get'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -48,7 +50,7 @@ SampleApp::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
